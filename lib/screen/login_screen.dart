de8e437d6cee9cf8e7513236de6a6d38b2789446
ASUS <mustafasharaf100@gmail.com>
2024-04-17 17:39:08 +0300

    fonts

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -20,7 +20,24 @@ class _LoginPageState extends State<LoginPage>{
   TextEditingController email = TextEditingController();
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
-  Future signInWithGoogle() async {
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+/* Future signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
 
@@ -39,7 +56,8 @@ class _LoginPageState extends State<LoginPage>{
         MaterialPageRoute(builder: (context) {
           return HomePage();
         }));
-  }
+  }*/
+
 
   @override
   Widget build(BuildContext context) {
